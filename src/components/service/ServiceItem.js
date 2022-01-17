9fcec21742d28641dc9f2dd4b592b2127e02b3f3
kenan <kulkenan212@gmail.com>
2022-01-17 13:43:01 +0300

    homeve testimo

---
+++ src/components/service/ServiceItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ServiceItem = (props) => {
+  const { image, title, description, more } = props;
+
+  return (
+    // {/* <!-- SERVICE ITEM START --> */}
+    <div className="home-service-item fluid-height">
+      <div className="home-service-details full-height">
+        <div className="home-service-image">
+          <img src={`assets/images/${image}`} alt="service" />
+        </div>
+        <div className="home-service-text">
+          <h3>{title}</h3>
+          <p>{description}</p>
+          {more && <Link to="/service-details">Read More +</Link>}
+        </div>
+      </div>
+    </div>
+    // {/* <!-- SERVICE ITEM END --> */}
+  );
+};
+
+export default ServiceItem;
