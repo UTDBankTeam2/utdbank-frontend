c52dac11ce0f1d7fd4a6bd5e0d702235cc13623b
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2022-01-17 07:43:41 +0000

    tmm

--- src/components/home/HomePricing.js
+++ src/components/home/HomePricing.js
@@ -3,20 +3,20 @@ import SectionTitle from "../common/SectionTitle";
 
 const HomePricing = () => {
   return (
-    <section class="home-pricing-section p-tb-100">
-      <div class="container">
+    <section className="home-pricing-section p-tb-100">
+      <div className="container">
         <SectionTitle
           title="Our Reasonable Pricing"
           description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
             eiusmod labore et dolore magnam aliquam quaerat voluptatem."
         />
 
-        <div class="home-pricing-content">
-          <div class="home-pricing-category">
+        <div className="home-pricing-content">
+          <div className="home-pricing-category">
             <table>
               <thead>
-                <tr class="orange-gradient">
-                  <th class="t-align-left">Pricing list</th>
+                <tr className="orange-gradient">
+                  <th className="t-align-left">Pricing list</th>
                   <th>Business main</th>
                   <th>Householder</th>
                   <th>Freelancer</th>
@@ -24,149 +24,149 @@ const HomePricing = () => {
               </thead>
               <tbody>
                 <tr>
-                  <td class="pricing-name blue-gradient">
+                  <td className="pricing-name blue-gradient">
                     <img src="assets/images/check-white.png" alt="check" />
                     Technical support over IRC
                   </td>
                   <td>
-                    <span class="price-availability availability-orange-dark">
-                      <i class="bx bx-check"></i>
+                    <span className="price-availability availability-orange-dark">
+                      <i className="bx bx-check"></i>
                     </span>
                   </td>
                   <td>
-                    <span class="price-availability availability-orange-dark">
-                      <i class="bx bx-check"></i>
+                    <span className="price-availability availability-orange-dark">
+                      <i className="bx bx-check"></i>
                     </span>
                   </td>
                   <td>
-                    <span class="price-availability availability-orange-dark">
-                      <i class="bx bx-check"></i>
+                    <span className="price-availability availability-orange-dark">
+                      <i className="bx bx-check"></i>
                     </span>
                   </td>
                 </tr>
                 <tr>
-                  <td class="pricing-name blue-gradient">
+                  <td className="pricing-name blue-gradient">
                     <img src="assets/images/check-white.png" alt="check" />
                     Control payout timing
                   </td>
                   <td>
-                    <span class="price-availability availability-orange-light">
-                      <i class="bx bx-x"></i>
+                    <span className="price-availability availability-orange-light">
+                      <i className="bx bx-x"></i>
                     </span>
                   </td>
                   <td>
-                    <span class="price-availability availability-orange-light">
-                      <i class="bx bx-x"></i>
+                    <span className="price-availability availability-orange-light">
+                      <i className="bx bx-x"></i>
                     </span>
                   </td>
                   <td>
-                    <span class="price-availability availability-orange-light">
-                      <i class="bx bx-x"></i>
+                    <span className="price-availability availability-orange-light">
+                      <i className="bx bx-x"></i>
                     </span>
                   </td>
                 </tr>
                 <tr>
-                  <td class="pricing-name blue-gradient">
+                  <td className="pricing-name blue-gradient">
                     <img src="assets/images/check-white.png" alt="check" />
                     Automate evidence submission
                   </td>
                   <td>
-                    <span class="price-availability availability-blue">
-                      <i class="bx bx-check"></i>
+                    <span className="price-availability availability-blue">
+                      <i className="bx bx-check"></i>
                     </span>
                   </td>
                   <td>
-                    <span class="price-availability availability-blue">
-                      <i class="bx bx-check"></i>
+                    <span className="price-availability availability-blue">
+                      <i className="bx bx-check"></i>
                     </span>
                   </td>
                   <td>
-                    <span class="price-availability availability-blue">
-                      <i class="bx bx-check"></i>
+                    <span className="price-availability availability-blue">
+                      <i className="bx bx-check"></i>
                     </span>
                   </td>
                 </tr>
                 <tr>
-                  <td class="pricing-name blue-gradient">
+                  <td className="pricing-name blue-gradient">
                     <img src="assets/images/check-white.png" alt="check" />
                     Collaboration notes
                   </td>
                   <td>
-                    <span class="price-availability availability-orange-dark">
-                      <i class="bx bx-check"></i>
+                    <span className="price-availability availability-orange-dark">
+                      <i className="bx bx-check"></i>
                     </span>
                   </td>
                   <td>
-                    <span class="price-availability availability-orange-dark">
-                      <i class="bx bx-check"></i>
+                    <span className="price-availability availability-orange-dark">
+                      <i className="bx bx-check"></i>
                     </span>
                   </td>
                   <td>
-                    <span class="price-availability availability-orange-dark">
-                      <i class="bx bx-check"></i>
+                    <span className="price-availability availability-orange-dark">
+                      <i className="bx bx-check"></i>
                     </span>
                   </td>
                 </tr>
                 <tr>
-                  <td class="pricing-name blue-gradient">
+                  <td className="pricing-name blue-gradient">
                     <img src="assets/images/check-white.png" alt="check" />
                     Transparent payouts
                   </td>
                   <td>
-                    <span class="price-availability availability-orange-light">
-                      <i class="bx bx-x"></i>
+                    <span className="price-availability availability-orange-light">
+                      <i className="bx bx-x"></i>
                     </span>
                   </td>
                   <td>
-                    <span class="price-availability availability-orange-light">
-                      <i class="bx bx-x"></i>
+                    <span className="price-availability availability-orange-light">
+                      <i className="bx bx-x"></i>
                     </span>
                   </td>
                   <td>
-                    <span class="price-availability availability-orange-light">
-                      <i class="bx bx-x"></i>
+                    <span className="price-availability availability-orange-light">
+                      <i className="bx bx-x"></i>
                     </span>
                   </td>
                 </tr>
                 <tr>
-                  <td class="pricing-name blue-gradient">
+                  <td className="pricing-name blue-gradient">
                     <img src="assets/images/check-white.png" alt="check" />
                     24×7 support
                   </td>
                   <td>
-                    <span class="price-availability availability-green">
-                      <i class="bx bx-check"></i>
+                    <span className="price-availability availability-green">
+                      <i className="bx bx-check"></i>
                     </span>
                   </td>
                   <td>
-                    <span class="price-availability availability-green">
-                      <i class="bx bx-check"></i>
+                    <span className="price-availability availability-green">
+                      <i className="bx bx-check"></i>
                     </span>
                   </td>
                   <td>
-                    <span class="price-availability availability-green">
-                      <i class="bx bx-check"></i>
+                    <span className="price-availability availability-green">
+                      <i className="bx bx-check"></i>
                     </span>
                   </td>
                 </tr>
                 <tr>
-                  <td class="pricing-name blue-gradient">
+                  <td className="pricing-name blue-gradient">
                     <img src="assets/images/check-white.png" alt="check" />
                     Cognitive Management
                   </td>
                   <td>
-                    <span class="price-availability availability-orange-dark">
-                      <i class="bx bx-check"></i>
+                    <span className="price-availability availability-orange-dark">
+                      <i className="bx bx-check"></i>
                     </span>
                   </td>
                   <td>
-                    <span class="price-availability availability-orange-dark">
-                      <i class="bx bx-check"></i>
+                    <span className="price-availability availability-orange-dark">
+                      <i className="bx bx-check"></i>
                     </span>
                   </td>
                   <td>
-                    <span class="price-availability availability-orange-dark">
-                      <i class="bx bx-check"></i>
+                    <span className="price-availability availability-orange-dark">
+                      <i className="bx bx-check"></i>
                     </span>
                   </td>
                 </tr>
